a2a1339c4233d6439daf6234e5b8f6358d4f98c5
Peter Brandel <peter.brandel@shopify.com>
2021-09-10 14:49:55 -0400

    Support both spin and isospin ...

--- install.sh
+++ install.sh
@@ -13,25 +13,30 @@ cp bash/bash_profile $HOME/.bash_profile
 cp -r bash/oh-my-bash $HOME/.oh-my-bash
 
 
-# dependencies
-if ! command -v rg &> /dev/null; then
-  sudo apt-get install -y ripgrep
+
+
+if [[ `systemctl` =~ -\.mount ]]; then
+  # Some setup specifc to ISOSPIN
+  nix-channel --update && nix upgrade-nix
+  nix-env -iA nixpkgs.neovim
+  nix-env -iA nixpkgs.fzf
+  nix-env -iA nixpkgs.ripgrep
+else
+  # Some setup specifc to SPIN
+  sudo apt-get install -y software-properties-common
+  sudo add-apt-repository -y ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install neovim
+
+  # dependencies
+  if ! command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+  fi
+
+  if ! command -v fzf &> /dev/null; then
+    sudo apt-get install -y fzf
+  fi
 fi
-
-if ! command -v fzf &> /dev/null; then
-  sudo apt-get install -y fzf
-fi
-
-
-# install some more recent versions of software I need via nix
-nix-channel --update && nix upgrade-nix
-nix-env -iA nixpkgs.neovim
-nix-env -iA nixpkgs.fzf
-
-# nvim >= 0.5.0 !!!
-# sudo apt-get install -y software-properties-common
-# sudo add-apt-repository -y ppa:neovim-ppa/unstable
-# sudo apt-get update
 
 sudo apt-get install -y python-dev python3-pip python3-dev python3-pip python3-neovim
 
