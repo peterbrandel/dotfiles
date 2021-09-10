751dca1ede2d74d8c47e91cbf63960fc0a0111ad
Peter Brandel <peter.brandel@shopify.com>
2021-09-10 13:18:04 -0400

    Adjusting dotfiles for isospin setup

--- install.sh
+++ install.sh
@@ -21,22 +21,28 @@ if ! command -v fzf &> /dev/null; then
 fi
 
 
-# nvim >= 0.5.0 !!!
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt-get update
+# install some more recent versions of software I need via nix
 
-sudo apt-get install -y neovim python-dev python3-pip python3-dev python3-pip python3-neovim
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.fzf
+# nvim >= 0.5.0 !!!
+# sudo apt-get install -y software-properties-common
+# sudo add-apt-repository -y ppa:neovim-ppa/unstable
+# sudo apt-get update
+
+sudo apt-get install -y python-dev python3-pip python3-dev python3-pip python3-neovim
 
 cp -r nvim ~/.config/nvim
 sh nvim/installer.sh $HOME/.cache/dein
-sudo gem install neovim
 python3 -m pip install neovim
 python3 -m pip install msgpack
 
-# linters
-sudo gem install solargraph
-sudo gem install sorbet
+# install gems for code checks
+
+for d in $HOME/src/github.com/Shopify/*; do
+  sudo gem install neovim
+  cd $d && gem install solargraph sorbet
+done
 
 # rg
 cp rg/rgignore $HOME/.rgignore
